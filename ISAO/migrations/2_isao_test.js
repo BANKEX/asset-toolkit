dc27e9d49486cd824aa64f802497ef568d3f9d20
Nikita Kozlov <nk@bankexfoundation.org>
2018-06-14 00:46:47 +0300

    fixes

--- ISAO/migrations/2_isao_test.js
+++ ISAO/migrations/2_isao_test.js
@@ -14,13 +14,31 @@ const MINIMAL_DEPOSIT_SIZE = tw(0.05)
 
 const TI_DAY = tbn(86400);
 
+const ST_DEFAULT = tbn(0x00);
+const ST_RAISING = tbn(0x01);
+const ST_WAIT_FOR_ICO = tbn(0x02);
+const ST_MONEY_BACK = tbn(0x04);
+const ST_TOKEN_DISTRIBUTION = tbn(0x08);
+const ST_FUND_DEPRECATED = tbn(0x10);
+
+
+const RL_DEFAULT = tbn(0x00);
+const RL_POOL_MANAGER = tbn(0x01);
+const RL_ICO_MANAGER = tbn(0x02);
+const RL_ADMIN = tbn(0x04);
+const RL_PAYBOT = tbn(0x08);
+
+
+
+
 
 
 module.exports = function(deployer, network, accounts) {
   const operator = accounts[0];
   const RAISING_PERIOD = TI_DAY.mul(10);
   const DISTRIBUTION_PERIOD = TI_DAY.mul(36524);
-  const MINIMAL_FUND_SIZE = tw(110);
+  // const MINIMAL_FUND_SIZE = tw(110);
+  const MINIMAL_FUND_SIZE = tw(10);
   const LIMITS = [tw(100), tw(500), tw(1000)];
   const COSTS = [tw(1), tw(2), tw(2.5)];
   const PAYBOT_ADDRESS = accounts[9];
@@ -65,8 +83,12 @@ module.exports = function(deployer, network, accounts) {
           let a = (await isao.tokenAddress()).toString();
           let b = (isao.address).toString();
 
+          // await isao.setState(ST_RAISING, {from: operator});
+          // let state = await isao.getState();
+
           console.log(`Address of ERC20 token: ${a}`);
           console.log(`Address of ISAO: ${b}`);
+          // console.log(`State of ISAO: ${state}`);
       });
 
 
