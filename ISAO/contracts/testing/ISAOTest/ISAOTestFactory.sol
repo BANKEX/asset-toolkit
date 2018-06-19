f7210775844a0dedaae846302573505ac754326d
Igor Gulamov <igor.gulamov@gmail.com>
2018-06-19 16:28:05 +0300

    factory update

--- ISAO/contracts/testing/ISAOTest/ISAOTestFactory.sol
+++ ISAO/contracts/testing/ISAOTest/ISAOTestFactory.sol
@@ -9,15 +9,37 @@ contract ERC20Test is ERC20 {
   string public name = "ISAO Token";
   string public symbol = "ISAOT";
   uint8 public decimals = 18;
-
-
-  constructor(address _owner, uint _totalSupply) public {
-    totalSupply_ = _totalSupply;
-    balances[_owner] = _totalSupply;
-    emit Transfer(address(0), _owner, _totalSupply);
+  
+  function mint(address _for, uint _value) public returns (bool) {
+    require(totalSupply_==0);
+    totalSupply_ = _value;
+    balances[_for] = _value;
+    emit Transfer(address(0), _for, _value);
+    return true;
   }
 }
 
+contract ISAOTest2 is ISAOTest {
+  uint8 constant ST_RAISING = 0x01;
+  constructor(uint _raisingPeriod, 
+              uint _distributionPeriod, 
+              uint _minimalFundSize,
+              uint[] _limits, 
+              uint[] _costs,
+              uint _minimalDeposit,
+              address _adminAddress,
+              address _paybotAddress) public ISAOTest(_raisingPeriod, _distributionPeriod,
+              _minimalFundSize, _limits, _costs, _minimalDeposit, _adminAddress, _paybotAddress
+              ) {
+  }
+  function init (address _tokenAddress) public returns(bool) {
+      require(tokenAddress==address(0));
+      tokenAddress=_tokenAddress;
+      initialState_ = ST_RAISING;
+      launchTimestamp = getTimestamp_();
+      return true;
+  }
+}
 
 /// @notice  This factory is not legacy, but it is ganache-compatible. Do not write in-constructor factories for production!
 contract ISAOTestFactory {
@@ -33,11 +55,12 @@ contract ISAOTestFactory {
               uint _minimalDeposit,
               address _adminAddress,
               address _paybotAddress) public {
-    isaoAddress = new ISAOTest(_raisingPeriod, _distributionPeriod, 
+                  
+    erc20Address = new ERC20Test();
+    isaoAddress = new ISAOTest2(_raisingPeriod, _distributionPeriod, 
       _minimalFundSize, _limits, _costs, _minimalDeposit, _adminAddress, _paybotAddress);
-    erc20Address = new ERC20Test(isaoAddress, _limits[_limits.length-1]);
-    ISAOTest(isaoAddress).setERC20Token(erc20Address); 
-    ISAOTest(isaoAddress).setState(ST_RAISING);
-    
+    ISAOTest2(isaoAddress).init(erc20Address);
+    ERC20Test(erc20Address).mint(isaoAddress, _limits[_limits.length-1]);
+
   }
 }
